03160b5637cd343693947c56e1a87d9908729d28
Raylu42x <bennett@atwoodzoo.com>
2025-11-17 19:16:36 -0800

    Create nav.js

---
+++ assets/nav.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const btn = document.querySelector('.nav-toggle');
+  const menu = document.querySelector('.nav-menu');
+
+  btn.addEventListener('click', () => {
+    menu.classList.toggle('open');
+  });
+});
